9f6144c82bec2911bc9a9f3116bcac10113078b0
Sergei-Eremin <serezhanv@gmail.com>
2022-09-29 11:43:09 +0300

    feat: server response typing, create interfaces

---
+++ YouTube-client-app/src/types/responseInterfaces.ts
@@ -0,0 +1,52 @@
+export interface IbaseResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: ResponseItem[];
+}
+
+export interface ResponseItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: Thumbnail;
+      medium: Thumbnail;
+      high: Thumbnail;
+      standard: Thumbnail;
+      maxres: Thumbnail;
+    };
+    channelTitle: string;
+    tags: string[];
+    categoryId: string;
+    liveBroadcastContent: string;
+    localized: {
+      title: string;
+      description: string;
+    };
+    defaultAudioLanguage: string;
+  };
+  statistics: Statistic;
+}
+
+export interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Statistic {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
