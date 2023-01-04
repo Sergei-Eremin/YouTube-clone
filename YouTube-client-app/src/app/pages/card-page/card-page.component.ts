3fe63c38a0d96c694ed2dcf71ad31b5fc357326e
Sergei-Eremin <serezhanv@gmail.com>
2023-01-04 15:17:54 +0300

    fix: реализовал отписку от http через экземпляр Subscription

--- YouTube-client-app/src/app/pages/card-page/card-page.component.ts
+++ YouTube-client-app/src/app/pages/card-page/card-page.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataRequestService } from 'src/app/services/data-request.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { format } from 'date-fns';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './card-page.component.html',
   styleUrls: ['./card-page.component.scss'],
 })
-export class CardPageComponent implements OnInit {
+export class CardPageComponent implements OnInit, OnDestroy {
   viewCount: string = '';
 
   likeCount: string = '';
@@ -25,29 +26,37 @@ export class CardPageComponent implements OnInit {
 
   date: string = '';
 
+  private _sub = new Subscription();
+
   constructor(private _dataRequest: DataRequestService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.params
-      .pipe(switchMap(({ id }) => this._dataRequest.requestCard(id)))
-      .subscribe((card) => {
-        if (!card) {
-          return;
-        }
+    this._sub.add(
+      this.route.params
+        .pipe(switchMap(({ id }) => this._dataRequest.requestCard(id)))
+        .subscribe((card) => {
+          if (!card) {
+            return;
+          }
 
-        this.viewCount = card?.statistics.viewCount;
-        this.likeCount = card?.statistics.likeCount;
-        this.dislikeCount = card?.statistics.dislikeCount;
-        this.commentCount = card?.statistics.commentCount;
-        this.image = card?.snippet.thumbnails.high.url;
-        this.description = card?.snippet.description;
-        this.title = card?.snippet.title;
-        this.date = `
-        ${format(new Date(card?.snippet.publishedAt), 'iiii')},
-        ${format(new Date(card?.snippet.publishedAt), 'LLLL')}
-        ${format(new Date(card?.snippet.publishedAt), 'dd')}
-        ${format(new Date(card?.snippet.publishedAt), 'yyyy')}
-      `;
-      });
+          this.viewCount = card?.statistics.viewCount;
+          this.likeCount = card?.statistics.likeCount;
+          this.dislikeCount = card?.statistics.dislikeCount;
+          this.commentCount = card?.statistics.commentCount;
+          this.image = card?.snippet.thumbnails.high.url;
+          this.description = card?.snippet.description;
+          this.title = card?.snippet.title;
+          this.date = `
+          ${format(new Date(card?.snippet.publishedAt), 'iiii')},
+          ${format(new Date(card?.snippet.publishedAt), 'LLLL')}
+          ${format(new Date(card?.snippet.publishedAt), 'dd')}
+          ${format(new Date(card?.snippet.publishedAt), 'yyyy')}
+        `;
+        }),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._sub.unsubscribe();
   }
 }
